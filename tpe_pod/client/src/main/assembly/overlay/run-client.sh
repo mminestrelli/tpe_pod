c1d15a4104b898bad03928b0aa57fc265db30faa
Agop Hurmuz <agophurmuz@gmail.com>
2017-10-23 20:20:35 -0300

    archetype done

---
+++ tpe_pod/client/src/main/assembly/overlay/run-client.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -cp 'lib/jars/*' "ar.edu.itba.tpe_pod.client.Client" $*
+
---
+++ tpe_pod/client/src/main/java/ar/edu/itba/tpe_pod/client/Client.java
@@ -0,0 +1,12 @@
+package ar.edu.itba.tpe_pod.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+    private static Logger logger = LoggerFactory.getLogger(Client.class);
+
+    public static void main(String[] args) {
+        logger.info("tpe_pod Client Starting ...");
+    }
+}
